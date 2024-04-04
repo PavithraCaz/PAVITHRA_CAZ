80020e2b709ba6540e29d5d22bac77704a6dfeb7
PavithraCaz <pavithracaz@gmail.com>
2024-04-04 15:02:13 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,71 @@
+ // Toggle button
+ if (document.getElementById('nav-mobile-btn')) {
+    document.getElementById('nav-mobile-btn').addEventListener('click', function () {
+        if (this.classList.contains('close')) {
+            document.getElementById('nav').classList.add('hidden');
+            this.classList.remove('close');
+        } else {
+            document.getElementById('nav').classList.remove('hidden');
+            this.classList.add('close');
+        }
+    });
+}
+// swiper
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  // Scroll to top
+let mybutton = document.getElementById("scrollBtn");
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+// Scroll to top
+// Get all sections that have an ID defined
+const sections = document.querySelectorAll("section[id]");
+
+// Add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  
+  // Get current scroll position
+  let scrollY = window.pageYOffset;
+  
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+    
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
+    }
+  });
+}
